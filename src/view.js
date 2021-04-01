076a0f189929400fc55c1f99bfd6b970399453fa
exuanbo <exuanbo@protonmail.com>
2021-04-01 09:48:50 +0100

    refactor: remove first draw delay

--- src/view.js
+++ src/view.js
@@ -4,18 +4,17 @@ const FPS = 60
 const FPS_INTERVAL = 1000 / FPS
 
 class View {
-  constructor() {
-    this.lastDrawTime = performance.now()
-  }
-
   draw(game) {
     window.requestAnimationFrame(() => this.draw(game))
+
     const currentTime = performance.now()
     const elapsed = currentTime - this.lastDrawTime
     if (!SPRITE.complete || elapsed <= FPS_INTERVAL) {
       return
     }
-    this.lastDrawTime = currentTime - (elapsed % FPS_INTERVAL)
+    this.lastDrawTime = isNaN(elapsed)
+      ? currentTime
+      : currentTime - (elapsed % FPS_INTERVAL)
 
     game.draw()
   }
