9cf1122fa2fef31dd85749fbd304794118fbb6b5
exuanbo <exuanbo@protonmail.com>
2021-04-01 22:14:46 +0100

    refactor: use js to size canvas

--- src/globals.js
+++ src/globals.js
@@ -2,9 +2,12 @@ export const CANVAS_SIZE = 320
 
 export const TILE_SIZE = 16
 
+const canvas = document.querySelector('canvas')
+canvas.height = canvas.width = CANVAS_SIZE
+
 const g = {
   assets: {},
-  ctx: document.querySelector('canvas').getContext('2d')
+  ctx: canvas.getContext('2d')
 }
 
 export default g
