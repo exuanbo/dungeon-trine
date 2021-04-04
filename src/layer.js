818095e480da352cd816461ba8fdc2142a5c3f3f
exuanbo <exuanbo@protonmail.com>
2021-04-04 11:03:52 +0100

    chore: add jsdoc link to `Layer`

--- src/layer.js
+++ src/layer.js
@@ -1,5 +1,9 @@
 import { View } from './view.js'
 
+/**
+ * {@link https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Optimizing_canvas#use_multiple_layered_canvases_for_complex_scenes
+ * |Use multiple layered canvases for complex scenes}
+ */
 export class Layer {
   /**
    * @param {number} [zIndex]
