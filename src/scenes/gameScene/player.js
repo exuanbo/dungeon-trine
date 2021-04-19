0e842096a8acf3faa24cee761e9443cc0e735763
exuanbo <exuanbo@protonmail.com>
2021-04-19 16:43:47 +0100

    feat(player): add `Wizard`

--- src/scenes/gameScene/player.js
+++ src/scenes/gameScene/player.js
@@ -27,3 +27,30 @@ export class Knight extends AttackerCharacter {
     this.isPlayer = true
   }
 }
+
+export class Wizard extends AttackerCharacter {
+  /**
+   * @param {import('./gameLayer').GameLayer} layer
+   */
+  constructor(layer) {
+    super(
+      layer,
+      /* attackerCharacterConfig */ {
+        animationsMap: createAnimationsMap(
+          /* animationDetailsMap */ data.animations.characters.wizard
+        ),
+        position: randomPosition(
+          /* horizontalOffset */ 64,
+          /* verticalOffset */ 64
+        ),
+        speed: 1,
+        attackInterval: 120
+      }
+    )
+
+    /**
+     * @override
+     */
+    this.isPlayer = true
+  }
+}
