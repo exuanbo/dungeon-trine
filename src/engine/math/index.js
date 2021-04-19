1caac28f7387853a8a7e65839008fbc604b80fbc
exuanbo <exuanbo@protonmail.com>
2021-04-19 08:59:42 +0100

    feat(engine): add `randomInt` to `math/index.js`

--- src/engine/math/index.js
+++ src/engine/math/index.js
@@ -1,3 +1,16 @@
 export * from './vector.js'
 
 export * from './box.js'
+
+/**
+ * {@link https://stackoverflow.com/a/1527820/13346012
+ * |Generating random whole numbers in JavaScript in a specific range?}
+ *
+ * @param {number} min
+ * @param {number} max
+ */
+export const randomInt = (min, max) => {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min)) + min
+}
